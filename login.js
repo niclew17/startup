d302aebb77f9e9272e6074c972f06a64793c8bbc
niclew17 <niclewis17@gmail.com>
2023-10-28 08:28:48 -0600

    added js to allow for datatable initilization

--- login.js
+++ login.js
@@ -5,7 +5,13 @@ function login() {
   
   localStorage.setItem("profileName", name.value);
   localStorage.setItem("profilePassword", password.value);
-
+  
+  const loginData = {
+    "Name": name,
+    "Password": password
+  };
+  const loginjsonData = JSON.stringify(data, null, 2);
+  localStorage.setItem("profileJSON", loginjsonData);
   window.location.href = "generator.html";
 }
 

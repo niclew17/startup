2b982548f497f65f3f5504254038195185a5c4c7
niclew17 <niclewis17@gmail.com>
2023-11-08 11:22:13 -0700

    added chatgpt completion api

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const app = express();
+
+// The service port. In production the front-end code is statically hosted by the service on the same port.
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+// JSON body parsing using built-in middleware
+app.use(express.json());
+
+// Serve up the front-end static content hosting
+app.use(express.static('public'));
+
+// Router for service endpoints
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+// GetGenerations
+apiRouter.get('/generation', (_req, res) => {
+  res.send(generations);
+});
+
+// SubmitGeneration
+apiRouter.post('/generation', (req, res) => {
+  scores = updateScores(req.body, generations);
+  res.send(generations);
+});
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+let generations = [ {
+    "name": "Thomas",
+    "job": "Manager",
+    "download": "Download"
+  },
+  {
+    "name": "Mark",
+    "job": "Salesman",  
+    "download": "Download"
+  },
+  {
+    "name": "Seth",
+    "job": "Receptionist",
+    "download": "Download"
+  },
+  {
+    "name": "Donald",
+    "job": "Driver",
+    "download": "Download"
+  },
+  {
+    "name": "Robert",
+    "job": "Secretary",
+    "download": "Download"
+  }];
+function updateGenerations(newGenerations, generations) {
+
+//   let found = false;
+//   for (const [i, prevScore] of scores.entries()) {
+//     if (newScore.score > prevScore.score) {
+//       scores.splice(i, 0, newScore);
+//       found = true;
+//       break;
+//     }
+//   }
+
+  if (!found) {
+    generations.push(newGenerations);
+  }
+
+//   if (scores.length > 10) {
+//     scores.length = 10;
+//   }
+
+  return generations;
+}
+
--- public/generator.js
+++ public/generator.js
@@ -1,9 +1,8 @@
-const API_KEY = "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";
 document.addEventListener("DOMContentLoaded", function () {
   const nameInput = document.getElementById("Name");
   const positionInput = document.getElementById("Position");
   const jobDescriptionInput = document.getElementById("JobDescription");
-  const fileInput = document.getElementById("File");
+  const resumeInput = document.getElementById("Resume");
   const outputTextarea = document.getElementById("output");
   const generateButton = document.getElementById("generateButton");
   
@@ -14,39 +13,44 @@ document.addEventListener("DOMContentLoaded", function () {
     const name = nameInput.value;
     const position = positionInput.value;
     const jobDescription = jobDescriptionInput.value;
-    const selectedFile = fileInput.files[0];
+    const resume = resumeInput.value;
     
     const data = {
       "Applicant Name": name,
       "Job Position": position,
       "Job Description": jobDescription,
-      "Selected File": selectedFile ? selectedFile.name : "No file selected",
+      "Resume": resume
     };
     const jsonData = JSON.stringify(data, null, 2);
-    outputTextarea.value = "This will get outputed to an External API and the received data will go here\n" + jsonData;
+    info = "Please take the following information and compare the Resume with the Job Description and the Job Position, give feedback as to if the candidate would perform well based off of the given information. Here is the information." + jsonData;
+    generateResponse(info);
   });
 });
 
-const generateResponse = (chatElement) => {
+const generateResponse = (userMessage) => {
   const API_URL = "https://api.openai.com/v1/chat/completions";
-  const messageElement = chatElement.querySelector("p");
-  // Define the properties and message for the API request
+  const messageElement = document.getElementById("output");
+
   const requestOptions = {
-      method: "POST",
-      headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${API_KEY}`
-      },
-      body: JSON.stringify({
-          model: "gpt-3.5-turbo",
-          messages: [{role: "user", content: userMessage}],
-      })
-  }
-  // Send POST request to API, get response and set the reponse as paragraph text
-  fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${API_KEY}`,
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: userMessage }],
+    }),
+  };
+
+  fetch(API_URL, requestOptions)
+    .then((res) => res.json())
+    .then((data) => {
       messageElement.textContent = data.choices[0].message.content.trim();
-  }).catch(() => {
+    })
+    .catch(() => {
       messageElement.classList.add("error");
       messageElement.textContent = "Oops! Something went wrong. Please try again.";
-  }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
-}
+    })
+    .finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
+};
